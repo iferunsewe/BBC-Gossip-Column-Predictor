ae78b244d3027aab27051e51e4c15e7b39b856aa
Ife Runsewe <iferunsewe@gmail.com>
2023-04-09 17:35:12 +0200

    Transfermarkt scraper

--- src/scrapers/transfermarkt_scraper.py
+++ src/scrapers/transfermarkt_scraper.py
@@ -1,86 +1,102 @@
-import requests
-from bs4 import BeautifulSoup
 import csv
+import sys
+import os
 
-url = "https://www.transfermarkt.com/premier-league/transfers/wettbewerb/GB1/saison_id/2021"
+# Add parent directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36"
-}
+from utils import scrape_content, get_data_file_path
 
-# Fetch the webpage content
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.content, "html.parser")
+# Process player data for a given row and h2_club
+def process_player_data(row, h2_club):
+    # Extract player details from the row
+    player_name = row.find_all("td")[0].find(class_="hide-for-small").text.strip()
+    player_age = row.find(class_="alter-transfer-cell").text.strip()
+    player_position = row.find(class_="pos-transfer-cell").text.strip()
 
-# Find tables matching the specified selector
-tables = soup.select('.box > .responsive-table > table')
+    # Extract club information for the player
+    club_left = process_club_data(row, h2_club, "Left")
+    club_joined = process_club_data(row, h2_club, "Joined")
 
-# Initialize transfer data and ID counter
-transfer_data = []
-id_counter = 385490
+    # Extract market value for the player
+    market_value = row.find(class_="rechts").text.strip()
 
-# Create the CSV file
-with open("../../data/transfermarkt_data.csv", "w", newline="", encoding="utf-8") as csvfile:
-    fieldnames = ["id", "player_name", "player_age", "player_position", "club_left", "club_joined", "transfer_fee"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+    return {
+        "player_name": player_name,
+        "player_age": player_age,
+        "player_position": player_position,
+        "club_left": club_left,
+        "club_joined": club_joined,
+        "market_value": market_value
+    }
 
-    print(f"Found {len(tables)} tables")
-    # Process each table
-    for table in tables: 
+# Process club data for a given row
+def process_club_data(row, h2_club, cell_text):
+    club_cell = row.find("th", class_="verein-transfer-cell", text=cell_text)
+    club_link = row.find(class_="verein-flagge-transfer-cell").find("a")
+
+    if club_cell and club_link:
+        club = club_link.text.strip()
+        if club == "Without Club":
+            return None
+    elif club_cell and not club_link:
+        return None
+    else:
+        club = h2_club
+
+    return club
+
+def process_transfer_data(soup, season):
+    # Find all transfer tables in the page
+    tables = soup.select('.box > .responsive-table > table')
+    transfer_data = []
+
+    # Iterate through each table and extract transfer data
+    for table in tables:
         table_rows = table.find("tbody").find_all("tr")
-
-        print(f"Found {len(table_rows)} rows in the table")
-        # Find club_left and club_joined from h2 element of the parent element "responsive-table"
         h2_sibling = table.find_parent("div", class_="responsive-table").find_previous_sibling("h2")
         clubs = h2_sibling.find_all("a")
         h2_club = clubs[1].text.strip()
-        print(f"Found h2 club: {h2_club}")
 
-        # Process each row in the table
+        # Iterate through each row and process player data
         for row in table_rows:
-            player_name = row.find_all("td")[0].find(class_="hide-for-small").text.strip()
-            player_age = row.find(class_="alter-transfer-cell").text.strip()
-            player_position = row.find(class_="pos-transfer-cell").text.strip()
+            transfer = process_player_data(row, h2_club)
+            transfer["season"] = season
+            if transfer["club_left"] and transfer["club_joined"]:
+                transfer_data.append(transfer)
 
-            club_left_cell = table.find("th", class_="verein-transfer-cell", text="Left")
-            club_left_link = row.find(class_="verein-flagge-transfer-cell").find("a")
+    return transfer_data
 
-            if club_left_cell and club_left_link:
-                club_left = club_left_link.text.strip()
-                if club_left == "Without Club":
-                    continue
-            elif club_left_cell and not club_left_link:
-                continue
-            else:
-                club_left = h2_club
+def save_transfer_data_to_csv(transfer_data, filename):
+    with open(get_data_file_path(filename), "w", newline="", encoding="utf-8") as csvfile:
+        fieldnames = ["id", "player_name", "player_age", "player_position", "club_left", "club_joined", "market_value", "season"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
-            club_joined_cell = table.find("th", class_="verein-transfer-cell", text="Joined")
-            club_joined_link = row.find(class_="verein-flagge-transfer-cell").find("a")
-            if club_joined_cell and club_joined_link:
-                club_joined = club_joined_link.text.strip()
-                if club_joined == "Without Club":
-                    continue
-            elif club_joined_cell and not club_joined_link:
-                continue
-            else:
-                club_joined = h2_club
-
-            transfer_fee = row.find(class_="rechts").text.strip()
-
-            transfer = {
-                "id": id_counter,
-                "player_name": player_name,
-                "player_age": player_age,
-                "player_position": player_position,
-                "club_left": club_left,
-                "club_joined": club_joined,
-                "transfer_fee": transfer_fee
-            }
-
-            # Save the data to the CSV file
+        for idx, transfer in enumerate(transfer_data, start=1):
+            transfer["id"] = idx
             writer.writerow(transfer)
-            print(f"Processed transfer {id_counter}: {player_name} from {club_left} to {club_joined}")
+            print(f"Saved transfer {idx}: {transfer['player_name']} from {transfer['club_left']} to {transfer['club_joined']} in {transfer['season']} season")
 
-            transfer_data.append(transfer)
-            id_counter += 1
+
+def main():
+    seasons = {
+        "2021": "https://www.transfermarkt.com/premier-league/transfers/wettbewerb/GB1/saison_id/2021",
+        "2022": "https://www.transfermarkt.com/premier-league/transfers/wettbewerb/GB1/saison_id/2022"
+    }
+
+    all_transfer_data = []
+
+    # Iterate through each season URL and fetch the transfer data
+    for season, url in seasons.items():
+        print(f"Fetching data for {season} season...")
+        soup = scrape_content(url)
+        transfer_data = process_transfer_data(soup, season)
+        all_transfer_data.extend(transfer_data)
+
+    # Save all transfer data to a CSV file
+    save_transfer_data_to_csv(all_transfer_data, "transfermarkt_data.csv")
+    print("Transfer data saved to transfermarkt_data.csv.")
+
+if __name__ == "__main__":
+    main()
