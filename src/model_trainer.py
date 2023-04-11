ce82fbff3a172b355530a4a6fd0b8c044e4d02d6
Ife Runsewe <iferunsewe@gmail.com>
2023-04-12 00:30:31 +0200

    Better model trainer

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -1,86 +1,120 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.metrics import accuracy_score, classification_report
-from sklearn.model_selection import GridSearchCV
 from xgboost import XGBClassifier
-import numpy as np
-from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.preprocessing import MultiLabelBinarizer, OneHotEncoder
+import utils
+from sklearn.impute import SimpleImputer
 
-# Load the final.csv dataset
-data = pd.read_csv("../data/final.csv")
+def load_data(filename):
+    return pd.read_csv(utils.get_data_file_path(filename))
 
-# Drop rows with NaN values in the 'veracity' column
-data = data.dropna(subset=['veracity'])
+def drop_na_veracity(data):
+    return data.dropna(subset=['veracity'])
 
-# Convert the 'clubs_mentioned' column from string to a list of clubs
-data['clubs_mentioned'] = data['clubs_mentioned'].apply(lambda x: eval(x))
+def convert_clubs_mentioned(data):
+    data.loc[:, 'clubs_mentioned'] = data['clubs_mentioned'].apply(
+        lambda x: eval(x) if x.startswith('[') else x.split(', '))
+    return data
 
-# One-hot encode the 'clubs_mentioned' column using MultiLabelBinarizer
-mlb = MultiLabelBinarizer()
-clubs_encoded = mlb.fit_transform(data['clubs_mentioned'])
-clubs_encoded_df = pd.DataFrame(clubs_encoded, columns=mlb.classes_)
+def clean_market_value(value):
+    if pd.isna(value) or value == '-':
+        return np.nan
 
-# Reset the index of the 'data' dataframe
-data.reset_index(drop=True, inplace=True)
+    value = value.replace('€', '')
+    if 'm' in value:
+        value = float(value.replace('m', '')) * 1_000_000
+    elif 'k' in value:
+        value = float(value.replace('k', '')) * 1_000
+    return value
 
-# Concatenate the original data with the one-hot encoded clubs_mentioned dataframe
-data_encoded = pd.concat([data, clubs_encoded_df], axis=1)
+def process_market_value(data):
+    data.loc[:, 'market_value'] = data['market_value'].apply(clean_market_value)
+    return data
 
-# Drop the 'news_id', 'player_name', and 'clubs_mentioned' columns
-X = data_encoded.drop(['news_id', 'player_name', 'clubs_mentioned', 'veracity'], axis=1)
+def encode_columns(data, columns_to_encode):
+    encoded_data = []
+    for col in columns_to_encode:
+        if col == 'clubs_mentioned':
+            mlb = MultiLabelBinarizer()
+            encoded = mlb.fit_transform(data[col])
+            encoded_df = pd.DataFrame(encoded, columns=mlb.classes_)
+        else:
+            ohe = OneHotEncoder(sparse=False)
+            encoded = ohe.fit_transform(data[[col]])
+            encoded_df = pd.DataFrame(encoded, columns=ohe.get_feature_names_out([col]))
+        
+        encoded_data.append(encoded_df)
+    return encoded_data
 
-# Use the 'veracity' column as the target variable
-y = data['veracity']
+def concat_encoded_data(data, encoded_data):
+    data.reset_index(drop=True, inplace=True)
+    data_encoded = pd.concat([data] + encoded_data, axis=1)
+    return data_encoded
 
-# Convert boolean values to integers
-y = y.astype(int)
+def preprocess_data(data):
+    data = drop_na_veracity(data)
+    data = convert_clubs_mentioned(data)
+    data = process_market_value(data)
+    columns_to_encode = ['clubs_mentioned', 'nationality', 'position', 'source']
+    encoded_data = encode_columns(data, columns_to_encode)
+    data_encoded = concat_encoded_data(data, encoded_data)
+    return data_encoded
 
-# Split the dataset into training and testing sets (80-20 split)
-print("Splitting the dataset into training and testing sets...")
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def drop_columns(data_encoded):
+    return data_encoded.drop(['date', 'id', 'clubs_mentioned', 'nationality', 'position', 'source', 'veracity'], axis=1)
 
-# Train and evaluate Random Forest Classifier
-rf = RandomForestClassifier(random_state=42)
-X_train = np.array(X_train)
-y_train = np.array(y_train)
-rf.fit(X_train, y_train)
-y_pred_rf = rf.predict(X_test)
-print("Random Forest accuracy:", accuracy_score(y_test, y_pred_rf))
-print(classification_report(y_test, y_pred_rf))
+def get_X_y(data, data_encoded):
+    data = data.dropna(subset=['veracity'])
+    X = drop_columns(data_encoded)
+    y = data['veracity'].astype(int)
+    return X, y
 
-# Train and evaluate AdaBoost Classifier
-ab = AdaBoostClassifier(random_state=42)
-ab.fit(X_train, y_train)
-y_pred_ab = ab.predict(X_test)
-print("AdaBoost accuracy:", accuracy_score(y_test, y_pred_ab))
-print(classification_report(y_test, y_pred_ab))
+def impute_missing_values(X):
+    imputer = SimpleImputer(strategy='mean')
+    X_imputed = imputer.fit_transform(X)
+    X_imputed = pd.DataFrame(X_imputed, columns=X.columns)
+    return X_imputed
 
-# Train and evaluate XGBoost Classifier
-xgb = XGBClassifier(random_state=42, use_label_encoder=False, eval_metric='mlogloss')
-xgb.fit(X_train, y_train)
-y_pred_xgb = xgb.predict(X_test)
-print("XGBoost accuracy:", accuracy_score(y_test, y_pred_xgb))
-print(classification_report(y_test, y_pred_xgb))
+def split_data(data, data_encoded):
+    X, y = get_X_y(data, data_encoded)
+    X_imputed = impute_missing_values(X)
+    X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.4, random_state=42)
+    return X_train, X_test, y_train, y_test
 
-# Perform hyperparameter optimization for XGBoost using GridSearchCV
-param_grid = {
-    'learning_rate': [0.01, 0.1],
-    'n_estimators': [100, 200],
-    'max_depth': [3, 5],
-    'subsample': [0.8, 1.0],
-    'colsample_bytree': [0.8, 1.0],
-}
+def train_and_evaluate_model(model, X_train, X_test, y_train, y_test):
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+    return accuracy, report
 
-xgb_opt = XGBClassifier(random_state=42, use_label_encoder=False, eval_metric='mlogloss')
-grid_search = GridSearchCV(xgb_opt, param_grid, cv=3, scoring='accuracy', n_jobs=-1)
-grid_search.fit(X_train, y_train)
+def print_model_results(model_name, accuracy, report):
+    print(f"{model_name} accuracy: {accuracy}")
+    print(report)
 
-print("Best parameters for XGBoost:", grid_search.best_params_)
-print("Best accuracy score:", grid_search.best_score_)
+def train_and_evaluate_models(X_train, X_test, y_train, y_test):
+    models = {
+        'Random Forest': RandomForestClassifier(random_state=42),
+        'AdaBoost': AdaBoostClassifier(random_state=42),
+        'XGBoost': XGBClassifier(random_state=42, use_label_encoder=False, eval_metric='mlogloss')
+    }
 
-# Evaluate the best model on the test set
-best_xgb = grid_search.best_estimator_
-y_pred_best_xgb = best_xgb.predict(X_test)
-print("Best XGBoost accuracy:", accuracy_score(y_test, y_pred_best_xgb))
-print(classification_report(y_test, y_pred_best_xgb))
+    for model_name, model in models.items():
+        accuracy, report = train_and_evaluate_model(model, X_train, X_test, y_train, y_test)
+        print_model_results(model_name, accuracy, report)
+        print_top_5_feature_importances(model_name, model, X_train)
+
+def print_top_5_feature_importances(model_name, model, X_train):
+    print(f"\n{model_name} top 5 features:")
+    feature_importances = sorted(zip(X_train.columns, model.feature_importances_), key=lambda x: x[1], reverse=True)
+    for feature, importance in feature_importances[:5]:
+        print(f"{feature}: {importance}")
+
+if __name__ == '__main__':
+    data = load_data("final_data.csv")
+    data_encoded = preprocess_data(data)
+    X_train, X_test, y_train, y_test = split_data(data, data_encoded)
+    train_and_evaluate_models(X_train, X_test, y_train, y_test)
